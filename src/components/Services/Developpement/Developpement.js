229a6379b1e54943744e7f753dd38e59401a6a5f
Frederic <frederic.nadaradjane@lagrandeclasse.fr>
2023-01-19 14:51:27 +0100

    structure Service

---
+++ src/components/Services/Developpement/Developpement.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Developpement() {
+  return (
+    <div>Developpement</div>
+  )
+}
+
+export default Developpement
---
+++ src/components/Services/Marketing/Marketing.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Marketing() {
+  return (
+    <div>Marketing</div>
+  )
+}
+
+export default Marketing
